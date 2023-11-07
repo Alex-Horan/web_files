f8eeb3b2b080ae3a3e0f38a1a73cc6eb69ed0024
Alex-Horan <alexandermhoran@gmail.com>
2023-11-07 14:32:15 -0500

    got dynamic page generation working. will now generate pages with the info given on input. still have to work on styling, security, and ux/accessibility

--- pageConstruct.php
+++ pageConstruct.php
@@ -1,35 +1,46 @@
 <?php
+session_start();
     class MyDB extends SQLite3 {
         public function __construct() {
             $this->open("./shop_items.sqlite");
         }
     }
+
+//function addInfo($stream, string $titleAdd, string $descAdd, float $priceAdd, string $templateAdd) {
+ //   fwrite($stream);
+//}
+
 class pageCreator {
 
-    public function create(string $title, string $desc, float $price, string $template) {
-        if(!mkdir("./$title")) {
-            die("Failed to make folder");
+    public function create(string $title, string $desc, float $price, string $template, string $addPhp) {
+        $title = str_replace(" ","_", $title);
+        $_SESSION['title'] = $title;
+        $_SESSION['descript'] = $desc;
+        $_SESSION['price'] = $price;
+        if(!mkdir("./item_pages/$title")) {
+            header("Location: ./index.php");
+            die("failed to create folder - ./item_pages/$title/$title");
         }
-            
-        $fw = fopen("./$title/$title","c");
+        if (is_dir("./item_pages/$title/$title")) {
+            rename("./item_pages/$title/$title", "./item_pages/err$title/err$title");
+        } else {
+        $fw = fopen("./item_pages/$title/$title","c");
+        fwrite($fw,"$addPhp");
         fwrite($fw,"$template");
-    }
-}
-    
-$newPage = new pageCreator();
-$db = new MyDB();
-$rdb = $db->query("SELECT * FROM listings");
-    while ($results = $rdb->fetchArray(SQLITE3_ASSOC)) {
-        $title = $results['name'];
-        $desc = $results['descript'];
-        $price = $results['price'];
-
-        foreach ($results as $key => $value) {
-            if ($key === 'name') {
-                echo $value;
-            }
+        rename("./item_pages/$title/$title","./item_pages/$title/$title.php");
         }
+        fclose($fw);
+        header("Location: ./item_pages/$title/$title.php");
     }
+    
+}
+
+$addPhp =" <?php 
+    session_start();
+    \$title = \$_SESSION['title'];
+    \$desc =  \$_SESSION['descript'];
+    \$price =  \$_SESSION['price'];
+?>";
 
 $template = "
     <!DOCTYPE html>
@@ -41,17 +52,31 @@ $template = "
     </head>
     <body>
         <div class='listings'>
-            <h1>$title</h1>
+            <h1><?php echo \$title; ?></h1>
             <br><br>
-            <p>$desc</p>
+            <p><?php echo \$desc; ?></p>
             <br><br>
-            <h3>$price</h3>
+            <h3><?php echo \$price; ?></h3>
         </div>
     
     
     </body>
     </html>
 ";
+
+$newPage = new pageCreator();
+$db = new MyDB();
+$rdb = $db->query("SELECT * FROM listings");
+    while ($results = $rdb->fetchArray(SQLITE3_ASSOC)) {
+        $title = $results['name'];
+        $desc = $results['descript'];
+        $price = $results['price'];
+
+        $newPage->create($title, $desc, $price, $template, $addPhp);
+        
+    }
+
+
 
 //testing
 
